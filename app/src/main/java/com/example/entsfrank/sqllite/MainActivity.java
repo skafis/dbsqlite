95aa5fc10b71b84423d3b3b79cb21b49d76ec5fc
skafis <franciskipchumba5@gmail.com>
2016-03-18 15:41:36 +0300

    check for database and create if not found

--- app/src/main/java/com/example/entsfrank/sqllite/MainActivity.java
+++ app/src/main/java/com/example/entsfrank/sqllite/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.entsfrank.sqllite;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -36,6 +37,26 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void createDatabase(View view) {
+        try {
+            contactsDB=this.openOrCreateDatabase("myDb",
+                    MODE_PRIVATE,null);
+            contactsDB.execSQL("Crete tableif not there " +
+                    "(id integer primary key, name VARCHAR, email VARCHAR);" );
+
+            File database = getApplicationContext().getDatabasePath("myDB.db");
+
+            if (!database.exists()){
+                Toast.makeText(this, "Data base created", Toast.LENGTH_SHORT).show();
+            }
+            else {
+                Toast.makeText(this, "No Databse", Toast.LENGTH_SHORT).show();
+
+            }
+        }
+        catch (Exception e ){
+            Log.e("mydb error", "error creating Database");
+        }
+
 
     }
 
